2413b6bc77d5e8560608dc0ffbe8da93c8a1d01c
obbap1 <paschal.obba@stu.cu.edu.ng>
2019-03-27 00:47:38 +0100

    Fix: validate websites or addresses

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 /* eslint-disable no-console */
 const chalk = require('chalk');
 const { prompt } = require('enquirer');
+const emoji = require('node-emoji');
+
 
 const kickStartApplication = async () => {
   console.log(chalk.blue('Welcome!, Find the server closest to you'));
+
+  // eslint-disable-next-line no-useless-escape
+  const regex = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/i;
 
   // Get number of servers
 
@@ -12,7 +17,7 @@ const kickStartApplication = async () => {
     name: 'numberOfServers',
     message: 'How many servers do you want to compare ? \n (Minimum Number: 1 Maximum Number: 5)',
     initial: '2',
-    validate: input => !isNaN(Number(input)) && Number(input) < 5 && Number(input) > 1,
+    validate: input => !isNaN(Number(input)) && Number(input) < 5 && Number(input) > 1 || 'Number of Servers should be between 2 - 5',
   };
 
   // Enter Server Addresses
@@ -22,18 +27,26 @@ const kickStartApplication = async () => {
     message: 'Enter the Websites / IP addresses of the servers',
     limit: 5,
     choices: null,
+    validate: form => Object.values(form).every(address => regex.test(address)) || 'Invalid IP/Web Address',
   };
 
   prompt(numberOfServers)
     .then((answer) => {
-      console.log(answer);
-      serverAddresses.choices = Array(Number(answer.numberOfServers)).fill('Enter Address:');
+      // console.log(answer);
+      serverAddresses.choices = Array(Number(answer.numberOfServers)).fill(`${emoji.find('computer').emoji}  Server`);
+
+      serverAddresses.choices.forEach((item, index, array) => {
+        // eslint-disable-next-line no-param-reassign
+        array[index] = `${item} ${index + 1}`;
+      });
+
       prompt(serverAddresses)
         .then((ans) => {
-          console.log(ans);
-        });
+          // console.log(ans);
+        })
+        .catch(e => console.error(e));
     })
-    .catch(console.error);
+    .catch(e => console.error(e));
 };
 
 kickStartApplication();
